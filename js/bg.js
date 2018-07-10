8b88c2326bd4041b2ca9c9cb1ad2c407c27cdf08
Mohammed Perves <pmuhamed10@gmail.com>
2018-07-10 02:41:59 -0400

    Add files via upload

---
+++ js/bg.js
@@ -0,0 +1,21 @@
+function changeBgAll() {
+	document.body.style.backgroundColor = "#202020";
+}
+function changeBgAnime(){
+    document.body.style.backgroundImage = "url('img/anime.jpg')";
+}
+function changeBgClassical() {
+    document.body.style.backgroundImage = "url('img/anime1.jpg')";
+}
+function changeBgModern() {
+    document.body.style.backgroundImage = "url('img/anime2.jpg')";
+}
+function changeBgNature(){
+    document.body.style.backgroundImage = "url('img/anime3.jpg')";
+}
+function changeBgPlaylists() {
+    document.body.style.backgroundImage = "url('img/anime4.jpg')";
+}
+
+
+
